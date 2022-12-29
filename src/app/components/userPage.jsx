4986e15dba03180f42047da657bd185136d4fa9e
Anton Peshekhonov <noviyya2021@gmail.com>
2022-12-29 18:18:02 +0300

    add button in userPage

--- src/app/components/userPage.jsx
+++ src/app/components/userPage.jsx
@@ -1,46 +1,48 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import {useParams} from "react-router-dom"
+import { useHistory, useParams } from "react-router-dom";
 import api from "../api";
-import Qualitie from "./qualitie";
 import QualitiesList from "./qualitieslist";
 
 const UserPage = () => {
-
-    const params = useParams()
-    const userId = params.userId
+    const history = useHistory();
+    const params = useParams();
+    const userId = params.userId;
     // console.log('userId',userId)
     // console.log('sliceUserId', userId.slice(1))
-    const [user, setUser] = useState()
+    const [user, setUser] = useState();
     useEffect(() => {
-        api.users.getById(userId.slice(1)).then((user) => setUser(user))
-    }, [])
-console.log('user', user)
+        api.users.getById(userId.slice(1)).then((user) => setUser(user));
+    }, []);
+    console.log("user", user);
 
-    const handleChangeSelectedUser = () => {
-        console.log('pushButton')
-    }
+    const handleSelectOtherUser = () => {
+        history.push("/users");
+        console.log("pushButton");
+    };
     return (
         <>
             {user && (
                 <div>
                     <h1>Имя: {user.name}</h1>
                     <h2>Профессия:{user.profession.name}</h2>
-                    <h3> {<QualitiesList qualities={user.qualities}/>}</h3>
+                    <h3> {<QualitiesList qualities={user.qualities} />}</h3>
                     <h5>CompletedMeetings: {user.completedMeetings}</h5>
                     <h2>Rate: {user.rate}</h2>
                 </div>
-                    )}
-                    <div>
-                        <button 
-                        className="btn btn-secondary mt-2"
-                        onClick={handleChangeSelectedUser}>
-                            Все пользователи
-                        </button>
-                    </div>
-                
+            )}
+            <div>
+                <button
+                    className="btn btn-secondary mt-2"
+                    onClick={() => {
+                        handleSelectOtherUser();
+                    }}
+                >
+                    Все пользователи
+                </button>
+            </div>
         </>
-    )
+    );
     // return "Loading...";
 };
 
